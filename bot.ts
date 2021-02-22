15fd69d895e6c15508ce1f567b7289f29f3393a4
Alex <arekusunaito@gmail.com>
2021-02-21 20:36:52 -0600

    Optimized Code for 1 command use as I wanted.

--- bot.ts
+++ bot.ts
@@ -11,7 +11,7 @@ const sequelizeInstance = new Sequelize('database','champion','generation5',
 let file = require('./secret.json');
 
 import {DidProbabilityHappen} from './virtues/Generation';
-import {Client as DiscordClient} from 'discord.js';
+import {Channel, Client as DiscordClient, TextChannel, User} from 'discord.js';
 const bot = new DiscordClient();
 const AGENT = file.AGENT;
 
@@ -19,7 +19,6 @@ bot.login(AGENT);
 
 const foundMessage = 'You Found a Shiny Pokemon!!';
 const failMessage = 'Sorry! You will have to try again!'
-
 const UserTable = sequelizeInstance.define('users',
 {
     discordID:
@@ -40,10 +39,29 @@ const UserTable = sequelizeInstance.define('users',
 
 });
 
-let resultMap:any =
+
+
+async function ExecuteCommand(command:string,messageAuthor:User,channel:TextChannel)
 {
-    "true":foundMessage,
-    "false":failMessage
+    let probabilityMap:{[commandName:string]:string}
+    probabilityMap["~encounter"]='1/8192'
+    probabilityMap["~neoEncounter"]='1/4096'
+    probabilityMap["~charmEncounter"]='3/8192'
+    probabilityMap["~neoCharmEncounter"]='3/4096'
+    if(command in probabilityMap && DidProbabilityHappen(probabilityMap[command]))
+    {
+        const data = 
+        {
+            discordID: messageAuthor.id,
+            // username: messageAuthor.username+messageAuthor.discriminator
+            username: `${messageAuthor.username}#${messageAuthor.discriminator}`
+        }
+        await UpdateOnShinyFound(data);
+        channel.send(foundMessage);
+    }
+    else channel.send(failMessage);
+    
+    
 }
 
 bot.once('ready',()=>
@@ -55,88 +73,7 @@ bot.on('message',async message=>
 {
     if(message.content.startsWith('~'))
     {
-        const data = 
-        {
-            discordID: message.author.id,
-            username: message.author.username+message.author.discriminator
-        }
-        if(message.content==='~encounter') //Generation 1-5
-        {
-            if(DidProbabilityHappen('1/8192'))
-            {
-                
-                await UpdateOnShinyFound(data);
-                message.channel.send(foundMessage);
-            }
-            else message.channel.send(failMessage);
-        }
-        
-        // neoEncounter
-        else if(message.content==='~neoEncounter') //Generation 6++
-        {
-            if(DidProbabilityHappen('1/4096'))
-            {
-                
-                await UpdateOnShinyFound(data);
-                message.channel.send(foundMessage);
-            }
-            else message.channel.send(failMessage);
-        }
-        // charmEncounter
-        else if(message.content==='~charmEncounter') //Generation 1-5 with Shiny Charm
-        {
-            if(DidProbabilityHappen('3/8192'))
-            {
-                
-                await UpdateOnShinyFound(data);
-                message.channel.send(foundMessage);
-            }
-            else message.channel.send(failMessage);
-        }
-        // neoCharmEncounter
-        else if(message.content==='~neoCharmEncounter') //Generation 6++ with Shiny Charm
-        {
-            if(DidProbabilityHappen('3/4096'))
-            {
-                
-                await UpdateOnShinyFound(data);
-                message.channel.send(foundMessage);
-            }
-            else message.channel.send(failMessage);
-        }
-
-        //Debug
-        else if(message.content==='~debugCheat')
-        {
-            if(DidProbabilityHappen('9/10'))
-            {
-                const data = 
-                {
-                    discordID: message.author.id,
-                    username: message.author.username+message.author.discriminator
-                }
-                await UpdateOnShinyFound(data);
-            }    
-            
-        }
-        else if(message.content==="~progress")
-        {
-            const foundUser = await UserTable.findOne({where:{discordID:message.author.id}})
-            if(foundUser)
-            {
-                message.channel.send(`${foundUser["username"]} you have found ${foundUser['shiniesFound']} Shinies`)
-            }
-            else
-            {
-                const data = 
-                {
-                    discordID: message.author.id,
-                    username: message.author.username+message.author.discriminator
-                }
-                await UserTable.create({discordID:data.discordID,username:data.username});
-                message.channel.send(`${data.username} you have found 0 Shinies`)
-            }
-        }
+        ExecuteCommand(message.content,message.author,message.channel as TextChannel);
     }
 });
 
